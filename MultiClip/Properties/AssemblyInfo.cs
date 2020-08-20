592b426c5c6553958f8afcbb95b219595239deda
oleg-shilo <oleg.shilo@gmail.com>
2020-08-20 23:26:48 +1000

    Release v1.2.0 - Project structure update:   - Multiclip -> Multiclip.Server   - Multiclip.UI -> Multiclip - Logging improvements - Addressed problem with uncontrolled restarting server on clipboard test failures - Fixed problem with invalid StartupScreenLocation when display scaling is not 100% - Added extra handling of system shutdown event

--- MultiClip/Properties/AssemblyInfo.cs
+++ MultiClip/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MultiClip.UI")]
+[assembly: AssemblyTitle("MultiClip")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Oleg Shilo")]
-[assembly: AssemblyProduct("MultiClip.UI")]
+[assembly: AssemblyProduct("MultiClip")]
 [assembly: AssemblyCopyright("Copyright © 2015-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.2.0")]
-[assembly: AssemblyFileVersion("1.1.2.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
