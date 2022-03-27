8f83c2f5d29020e3a52921887cdce6b3a3c2ae8c
oleg.shilo <oleg.shilo@gmail.com>
2022-03-27 14:59:24 +1100

    Pushed to choco v1.4.2.0

--- choco/tools/chocolateyInstall.ps1
+++ choco/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = 'multiclip'
-$url = 'https://github.com/oleg-shilo/multiclip/releases/download/v1.4.1.1/multiclip.v1.4.1.1.7z'
+$url = 'https://github.com/oleg-shilo/multiclip/releases/download/v1.4.2.0/multiclip.v1.4.2.0.7z'
 
 # In order to avoid multiclip app popping up message boxes need to indicate that
 # we are running under choco runtime by setting `UNDER_CHOCO` environment variable 
@@ -14,7 +14,7 @@ Stop-Process -Name "multiclip.server" -ErrorAction SilentlyContinue
 
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$cheksum = 'C4E1759F12C7233F91E34639517392EB4B728C2D626784007B78C8061DED6456'
+$cheksum = '9AC2B931E88E6487A39945E260562D60169B75DF279ABD77AFB14B9A3BCEC744'
 $checksumType = "sha256"
 
 # Download and unpack a zip file
--- choco/update_package.cs
+++ choco/update_package.cs
@@ -9,12 +9,12 @@ using System;
 
 //void main()
 //{
-    Console.WriteLine("Starting...");
+Console.WriteLine("Starting...");
 
-    ServicePointManager.Expect100Continue = true;
-    ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+ServicePointManager.Expect100Continue = true;
+ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
-    var url = "https://github.com/oleg-shilo/multiclip/releases/download/v1.4.1.1/multiclip.v1.4.1.1.7z";
+var url = "https://github.com/oleg-shilo/multiclip/releases/download/v1.4.2.0/multiclip.v1.4.2.0.7z";
 
 var installScript = @"tools\chocolateyInstall.ps1";
 
@@ -22,16 +22,16 @@ var cheksum = calcChecksum(url);
 // var cheksum = "E1809AD6433A91B2FF4803E7F4B15AE0FA88905A28949EAC5590F7D9FD9BE9C3";
 Console.WriteLine(cheksum);
 
-    var code = File.ReadAllText(installScript + ".template")
-                   .Replace("$url = ???", "$url = '" + url + "'")
-                   .Replace("$checksum = ???", "$cheksum = '" + cheksum + "'");
+var code = File.ReadAllText(installScript + ".template")
+               .Replace("$url = ???", "$url = '" + url + "'")
+               .Replace("$checksum = ???", "$cheksum = '" + cheksum + "'");
 
 File.WriteAllText(installScript, code);
-    Console.WriteLine("--------------");
-    Console.WriteLine(code);
-    Console.WriteLine("--------------");
-    Console.WriteLine();
-    Console.WriteLine("Done...");
+Console.WriteLine("--------------");
+Console.WriteLine(code);
+Console.WriteLine("--------------");
+Console.WriteLine();
+Console.WriteLine("Done...");
 //}
 
 string calcChecksum(string url)
